4bdec790493a144c91f01b8432a134644b49a297
Sergey <barclays1313@gmail.com>
2020-10-23 01:44:48 +0300

    feat(develop): implement resume to doc

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ let src = document.querySelector(".photo-person-src"),
 
 
 const showImage = (src,target) => {
-
     const fr = new FileReader();
     fr.onload = () => target.src = fr.result;
 
@@ -38,20 +37,62 @@ const additionalInformation = () => {
               <input class="info-preson-block-title" name="title" type="text" required placeholder="Название блока">
               <button class="add-description">+ Добавить описание </button>
               <div class="info-preson-block-description">
-                  <input class="info-preson-block-description-key" name="key" type="text" required placeholder="Заголовок поля...">
-                  <input class="info-preson-block-description-propetry" name="propetry" type="text" required placeholder="Содержание поля...">
+                  <input class="info-preson-block-description-key" name="key" type="text" required placeholder="Заголовок поля:">
+                  <input class="info-preson-block-description-propetry" name="propetry" type="text" required placeholder="Содержание поля:">
               </div>
             </div>`)
     } else if (target.className === "add-description") {
       target.insertAdjacentHTML('afterend', `
         <div class="info-preson-block-description">
-          <input class="info-preson-block-description-key" name="key" type="text" required placeholder="Заголовок поля...">
-          <input class="info-preson-block-description-propetry" name="propetry" type="text" required placeholder="Содержание поля...">
+          <input class="info-preson-block-description-key" name="key" type="text" required placeholder="Заголовок поля:">
+          <input class="info-preson-block-description-propetry" name="propetry" type="text" required placeholder="Содержание поля:">
         </div>`
       )
     }
   })
 }
+
+const removeElement = (element) => element.parentNode.removeChild(element);
+
+const saveDoc = () => {
+  var source = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(document.body.innerHTML);
+  var fileDownload = document.createElement("a");
+  document.body.appendChild(fileDownload);
+  fileDownload.href = source;
+  fileDownload.download = 'resume.doc';
+  fileDownload.click();
+  document.body.removeChild(fileDownload);
+}
+
+btnSaveResume.addEventListener('click', () => {
+
+  const getAllInputs = document.querySelectorAll('input'),
+    getBtns = document.querySelectorAll("button"),
+    getPhoto = document.querySelector(".photo-person");
+
+  for (let i = 0; i< getAllInputs.length ;i++) {
+
+    const newSpan = document.createElement('span'),
+        newH4 = document.createElement('h4');
+
+    newH4.textContent = getAllInputs[i].placeholder;
+    getAllInputs[i].before(newH4);
+
+    newSpan.style = "margin: auto; text-align: center";
+    getAllInputs[i].before(newSpan);
+
+     getAllInputs[i].value ? newSpan.textContent = getAllInputs[i].value : newSpan.textContent = 'Пустое поле...'
+
+    removeElement(getAllInputs[i]);
+  }
+
+  for (let i = 0; i< getBtns.length ;i++) {
+    removeElement(getBtns[i]);
+  }
+
+  removeElement(getPhoto);
+  saveDoc();
+})
 
 showImage(src,target);
 validity(); 
